9e1c85b51c77413021e2a2f6ee5cf9a4dd2bf637
Hi-Ayush <ayush.kumar.ee20@heritageit.edu.in>
2020-12-20 01:44:32 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,64 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 // your code goes here
+const students = require("./InitialData");
+let uniqueId= students.length+1;
+app.get("/api/student",(req,res)=>{
+    res.send(students);
+})
+app.get("/api/student/:id",(req,res)=>{
+    const id=req.params.id;
+    const studentData=students.find(student=>student.id===parseInt(id));
+    console.log(studentData);
+    if(!studentData){
+        res.status(404).send("Invalid id");
+        return;
+    }
+    res.send(studentData);
+    
+})
 
+app.post("/api/student",(req,res)=>{
+    const {name,currentClass,division}=req.body;
+    if(!(name||currentClass||division)){
+        res.status(400).send();
+        return;
+    }
+    const newStudent={
+        id:uniqueId++,
+        name,
+        currentClass:parseInt(currentClass),
+        division
+    }
+    students.push(newStudent);
+    res.send({id:newStudent.id})
+})
+
+app.put("/api/student/:id",(req,res)=>{
+    const id= req.params.id;
+    const newName=req.body.name;
+    const studentDataIndex=students.findIndex(student=>student.id===parseInt(id));
+    if(studentDataIndex===-1){
+        res.status(404).send("Invalid id");
+        return;
+    }
+    if(!newName){
+        res.status(404).send("Invalid Name");
+        return;
+    }
+    students[studentDataIndex].name===newName;
+    res.send(students[studentDataIndex]);
+})
+app.delete("/api/student/:id",(req,res)=>{
+    const id=parseInt(req.params.id);
+    const studentData=students.find(student=>student.id===parseInt(id));
+    if(!studentData){
+        res.status(400).send("Could not Delete As id is not found");
+        return;
+    }
+    students.splice(studentData,1);
+    res.send().status(200);
+})
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
